aeb320abed7dd5e7521eb66bb4bd76bc09dec66d
Evan Rose <evanandrewrose@gmail.com>
2024-04-14 15:54:36 -0400

    Fix leaderboard name search response type.

--- src/response-types/v1/leaderboard-name-search.ts
+++ src/response-types/v1/leaderboard-name-search.ts
@@ -1,33 +1,14 @@
 import { z } from "zod";
 
 export const LeaderboardNameSearchResponseSchema = z.object({
-  gamemodes: z.object({ 1: z.object({ name: z.string() }) }),
-  gateways: z.record(
-    z.string().regex(/^\d+$/),
-    z.object({
-      is_official: z.boolean(),
-      name: z.string(),
-      region: z.string(),
-    })
-  ),
-  leaderboards: z.record(
-    z.string().regex(/^\d+$/),
-    z.object({
-      benefactor_id: z.string(),
-      gamemode_id: z.number(),
-      gateway_id: z.number(),
-      id: z.number(),
-      last_update_time: z.string(),
-      name: z.string(),
-      next_update_time: z.string(),
-      program_id: z.string(),
-      season_id: z.number(),
-      season_name: z.string(),
-    })
-  ),
-  matchmaked_current_season: z.number(),
-  team_leaderboard_info: z.object({}),
-});
+  avatar: z.string(),
+  battletag: z.string(),
+  gateway_id: z.number(),
+  last_rank: z.number(),
+  name: z.string(),
+  points: z.number(),
+  rank: z.number(),
+}).array();
 
 export type LeaderboardNameSearchResponse = z.infer<
   typeof LeaderboardNameSearchResponseSchema
